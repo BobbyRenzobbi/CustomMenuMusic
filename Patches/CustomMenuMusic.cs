e651f68567a284a43d2eb775a5a86cfa3a1b0944
Rayd Zain <rzain200@gmail.com>
2024-08-11 17:30:42 -0400

    First commit

---
+++ Patches/CustomMenuMusic.cs
@@ -0,0 +1,77 @@
+﻿using SPT.Reflection.Patching;
+using EFT.UI;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using HarmonyLib;
+using Comfort.Common;
+
+namespace BobbyRenzobbi.CustomMenuMusic
+{
+    public class CustomMusicPatch : ModulePatch
+    {
+        private static int rndNumber = 0;
+        public static Dictionary<string, AudioClip> tracks = new Dictionary<string, AudioClip>();
+        private static List<AudioClip> audioClips = new List<AudioClip>();
+        private static System.Random rand = new System.Random();
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GUISounds), nameof(GUISounds.method_3));
+        }
+
+        private void LoadAudioClips()
+        {
+            string[] musicTracks = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\BepInEx\\plugins\\CustomMenuMusic\\music");
+
+            audioClips.Clear();
+            rndNumber = rand.Next(musicTracks.Length);
+            string fileDir = musicTracks[rndNumber];
+            var clip = RequestAudioClip(fileDir);
+            clip.name = Path.GetFileName(fileDir);
+            audioClips.Add(clip);
+            Logger.LogInfo(clip.name + " added to audioClips");
+        }
+
+        private AudioClip RequestAudioClip(string path)
+        {
+            string extension = Path.GetExtension(path);
+            Dictionary<string, AudioType> audioType = new Dictionary<string, AudioType>
+            {
+                [".wav"] = AudioType.WAV,
+                [".ogg"] = AudioType.OGGVORBIS,
+                [".mp3"] = AudioType.MPEG
+            };
+            UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(path, audioType[extension]);
+            UnityWebRequestAsyncOperation sendWeb = uwr.SendWebRequest();
+
+            while (!sendWeb.isDone)
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Logger.LogError("CustomMenuMusic Mod: Failed To Fetch Audio Clip");
+                return null;
+            }
+            AudioClip audioclip = DownloadHandlerAudioClip.GetContent(uwr);
+            return audioclip;
+        }
+
+        [PatchPrefix]
+        static bool Prefix()
+        {
+            CustomMusicPatch patch = new CustomMusicPatch();
+            patch.LoadAudioClips();
+            //Credit to SamSWAT for discovering that the game loads infinitely if the audioClip_0 array has only one element
+            if (audioClips.Count == 1)
+            {
+                audioClips.Add(audioClips[0]);
+            }
+            Traverse.Create(Singleton<GUISounds>.Instance).Field("audioClip_0").SetValue(audioClips.ToArray());
+            Logger.LogInfo("Playing " + audioClips[0].name);
+            return true;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System.IO;
+using System;
+using System.Linq;
+
+namespace BobbyRenzobbi.CustomMenuMusic
+{
+    [BepInPlugin("BobbyRenzobbi.CustomMenuMusic", "CustomMenuMusic", "0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        CustomMusicPatch patch = new CustomMusicPatch();
+        internal static ManualLogSource LogSource;
+        internal static string[] clips;
+        public static string[] GetTrack()
+        {
+            return Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\BepInEx\\plugins\\CustomMenuMusic\\music").Select(file => Path.GetFileName(file)).ToArray();
+        }
+        private void Awake()
+        {
+            clips = GetTrack();
+            LogSource = Logger;
+            new CustomMusicPatch().Enable();
+        }
+    }
+}
